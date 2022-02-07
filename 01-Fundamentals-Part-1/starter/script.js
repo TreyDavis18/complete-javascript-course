e2119b6c49641eadad87be263b945fbf0a9b7e74
Trey Davis <trey.davis0018@gmail.com>
2022-02-06 22:13:09 -0600

    Update

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+let firstName = "Trey";
+console.log(firstName);
+
+let country = "United States of America";
+let continent = "North America";
+let population = "330,000,000";
+
+console.log(country, ",", continent, ",", population);
